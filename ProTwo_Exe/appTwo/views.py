b34af072960b3c818efde77cc0414ab160e210e7
kyle <yangkou19@gmail.com>
2017-09-22 11:17:58 -0400

    FormValidation

--- ProTwo_Exe/appTwo/views.py
+++ ProTwo_Exe/appTwo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from appTwo.models import User
+# from appTwo.models import User
+from appTwo.forms import NewUserForm
 # Create your views here.
 
 def index(request):
@@ -7,6 +8,21 @@ def index(request):
 
 def users(request):
 
-    user_list = User.objects.order_by('last_name')
-    user_dict = {'users': user_list}
-    return render(request, 'appTwo/users.html', context=user_dict)
+    form = NewUserForm()
+
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('ERROR FORM INVALID')
+
+    return render(request,'appTwo/users.html', {'form':form})
+
+
+
+    # user_list = User.objects.order_by('last_name')
+    # user_dict = {'users': user_list}
+    # return render(request, 'appTwo/users.html', context=user_dict)
